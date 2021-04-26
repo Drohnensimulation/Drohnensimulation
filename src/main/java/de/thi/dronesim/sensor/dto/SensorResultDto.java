14afac655240268a7f74ff94b63f0132a4d1ebdd
johanne.steierl <johannes.steierl@geekinbusiness.de>
2021-04-26 17:57:59 +0200

    getter and setter for SensorResultDto

--- src/main/java/de/thi/dronesim/sensor/dto/SensorResultDto.java
+++ src/main/java/de/thi/dronesim/sensor/dto/SensorResultDto.java
@@ -9,4 +9,28 @@ public class SensorResultDto {
     private ASensor sensor;
     private Obstacle obstacle;
     private Set<Float> values;
+
+    public ASensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(ASensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public Obstacle getObstacle() {
+        return obstacle;
+    }
+
+    public void setObstacle(Obstacle obstacle) {
+        this.obstacle = obstacle;
+    }
+
+    public Set<Float> getValues() {
+        return values;
+    }
+
+    public void setValues(Set<Float> values) {
+        this.values = values;
+    }
 }
