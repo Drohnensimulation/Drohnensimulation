d5a50c3da3e87888309e4c4dcdf1cda578390670
Marvin <marvin.wittschen@gmx.de>
2021-05-17 03:29:52 +0200

    Adds test case for event listeners

--- src/test/java/de/thi/dronesim/SimulationTest.java
+++ src/test/java/de/thi/dronesim/SimulationTest.java
@@ -2,6 +2,8 @@ package de.thi.dronesim;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimulationTest {
@@ -18,7 +20,7 @@ public class SimulationTest {
         }
 
         @Override
-        public void setSimulation(Simulation simulation) {
+        public void initialize(Simulation simulation) {
             this.simulation = simulation;
         }
 
@@ -63,4 +65,32 @@ public class SimulationTest {
         MyTestChild childA = simulation.getChild(MyTestChild.class);
         assertNotNull(childA.getSimulation());
     }
+
+    @Test
+    void registerUpdateHandler() throws InterruptedException {
+        Simulation simulation = new Simulation();
+        simulation.prepare();
+
+        AtomicInteger i = new AtomicInteger();
+
+        SimulationUpdateListener l1 = event -> assertEquals(1, i.incrementAndGet());
+        simulation.registerUpdateListener(l1, 1000);
+        SimulationUpdateListener l2 = event -> assertEquals(2, i.incrementAndGet());
+        simulation.registerUpdateListener(l2, 800);
+        SimulationUpdateListener l3 = event -> assertEquals(3, i.incrementAndGet());
+        simulation.registerUpdateListener(l3, 10);
+        SimulationUpdateListener l4 = event -> assertEquals(4, i.getAndSet(0));
+        simulation.registerUpdateListener(l4, 10);
+
+        assertEquals(6, simulation.getUpdateListeners().size());
+        assertSame(l1, simulation.getUpdateListeners().get(1000));
+        assertSame(l2, simulation.getUpdateListeners().get(799));
+        assertSame(l3, simulation.getUpdateListeners().get(10));
+        assertSame(l4, simulation.getUpdateListeners().get(9));
+
+        simulation.start();
+        Thread.sleep(1000);
+        simulation.stop();
+    }
+
 }
