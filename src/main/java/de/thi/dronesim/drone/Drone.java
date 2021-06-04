83cd7b223725d8b8f6592ed204af00c97675d451
Bakri Aghyourli <b.aguorli@gmail.com>
2021-06-04 19:42:13 +0200

    added drone hitbox radius for collision detection

--- src/main/java/de/thi/dronesim/drone/Drone.java
+++ src/main/java/de/thi/dronesim/drone/Drone.java
@@ -7,17 +7,41 @@ public class Drone {
      * A Drone has a sort of Location State
      */
     private final Location location;
+    /**
+     * Drone's spherical hitbox radius for collision detection
+     */
+    private float radius;
 
+    /**
+     * default (0, 0, 0, 1)
+     */
     public Drone() {
         location = new Location(0, 0, 0);
+        radius = 1f;
     }
-
+    
+    /**
+     * radius default value is 1
+     * 
+     * @deprecated use {@link #Drone(x, y, z, radius)} to manually define hitbox radius instead.
+     */
+    @Deprecated
     public Drone(float x, float y, float z) {
         location = new Location(x, y, z);
+        this.radius = 1f;
+    }
+    
+    public Drone(float x, float y, float z, float radius) {
+    	location = new Location(x, y, z);
+    	this.radius = radius;
     }
 
     public Location getLocation() {
         return location;
     }
+    
+    public float getRadius() {
+		return radius;
+	}
 
 }
