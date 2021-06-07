b8afaea4c7c3f572637b02686e9e878ef6ba4efb
Daniel Dunger <dad5056@thi.de>
2021-06-08 00:12:45 +0200

    Testing

--- src/test/java/de/thi/dronesim/system/SystemTest.java
+++ src/test/java/de/thi/dronesim/system/SystemTest.java
@@ -15,55 +15,65 @@ public class SystemTest {
     @Test
     @Order(1)
     void TestSimulationDView()  {
+        // Start Position: (10, 0, 10) -> Goal (-10, 1, -10)
         Simulation sim = new Simulation("src/test/java/de/thi/dronesim/system/testconfig_1.json");
 
         sim.prepare();
         GuiManager gui = sim.getChild(GuiManager.class);
         assertNotNull(gui, "Could not get GuiManager.");
 
-        sim.start();
         gui.openDViewGui();
 
+        // Just waiting so it can properly load in, since it auto-starts atm.
         try{
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        sim.start();
+
         // Normally, an autopilot should be configurable to pilot towards the target destination
         Location l = sim.getDrone().getLocation();
-
         assertNotNull(l, "Could not get Location of Drone.");
 
+        // Take off
         l.requestDeltaVerticalSpeed(1);
-        while(l.getPosition().y <= 2.45f) {
-            // Wait until drone has reached the specified height. Autopilot should be more clever and use wait()
-            // While requestDeltaX actually notifies the caller once it has reached the desired value.
-        }
-        l.requestDeltaVerticalSpeed(-1);
-        while(l.getDeltaVerticalSpeed() != 0)
-        assertEquals(l.getPosition().y, 2.5f, 0.05);
-        l.setY(0);
+        while(l.getY() <= 4.4f) {}
+        l.requestDeltaVerticalSpeed(-0.9);
+        while(l.getY() <= 4.995f) {}
+        l.requestDeltaVerticalSpeed(-0.1);
 
-        l.requestDeltaHeading(-90);
-        while(l.getHeading() - 0.05 < 90 || l.getHeading() + 0.05 < 90) {
-        }
-        assertEquals(l.getHeading(), 270);
+        // Heading
+        l.requestDeltaHeading(135);
+        while(l.getHeading() != 135) {}
 
+        // Speed up towards target:
         l.requestDeltaAirspeed(2);
-        while(l.getPosition().x < 3.5)
-        {}
-        assertEquals(l.getPosition().x, 3.5, 0.05);
+        while(l.getPosition().x >= -8 || l.getPosition().z >= -8) {}
 
+        // Slow down towards Target
         l.requestDeltaAirspeed(-1.5);
-        l.requestDeltaVerticalSpeed(-0.25);
+        while(l.getPosition().x >= -9.5 || l.getPosition().z >= -9.5) {}
 
-        while(l.getPosition().y > 0.04) {}
-        l.requestDeltaVerticalSpeed(0.25);
+        l.requestDeltaAirspeed(-0.4);
+        while(l.getPosition().x >= -10 || l.getPosition().z >= -10) {}
 
-        assertEquals(l.getPosition().x, 3.5, 0.05);
-        assertEquals(l.getPosition().y, 0, 0.05);
-        assertEquals(l.getPosition().z, 0, 0.01);
+        l.requestDeltaAirspeed(-0.1);
+
+        l.requestDeltaVerticalSpeed(-0.5);
+        l.requestDeltaHeading(0);
+        while(l.getPosition().y >= 1.5) {}
+
+        l.requestDeltaVerticalSpeed(0.4);
+        while(l.getPosition().y >= 1.05) {}
+
+        l.requestDeltaVerticalSpeed(0.1);
+
         System.out.println("Test Finished: DView");
+
+        // Keep open:
+        while(true) {}
     }
 
     // Test without graphical output. Basically the same as above!
@@ -80,6 +90,55 @@ public class SystemTest {
         gui.openMViewGui();
 
         // Below is pretty much the same as above, just without the graphical output.
-        // Result should be the same
+        // Just waiting so it can properly load in, since it auto-starts atm.
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        sim.start();
+
+        // Normally, an autopilot should be configurable to pilot towards the target destination
+        Location l = sim.getDrone().getLocation();
+        assertNotNull(l, "Could not get Location of Drone.");
+
+        // Take off
+        l.requestDeltaVerticalSpeed(1);
+        while(l.getY() <= 4.4f) {}
+        l.requestDeltaVerticalSpeed(-0.9);
+        while(l.getY() <= 4.995f) {}
+        l.requestDeltaVerticalSpeed(-0.1);
+
+        // Heading
+        l.requestDeltaHeading(135);
+        while(l.getHeading() != 135) {}
+
+        // Speed up towards target:
+        l.requestDeltaAirspeed(2);
+        while(l.getPosition().x >= -8 || l.getPosition().z >= -8) {}
+
+        // Slow down towards Target
+        l.requestDeltaAirspeed(-1.5);
+        while(l.getPosition().x >= -9.5 || l.getPosition().z >= -9.5) {}
+
+        l.requestDeltaAirspeed(-0.4);
+        while(l.getPosition().x >= -10 || l.getPosition().z >= -10) {}
+
+        l.requestDeltaAirspeed(-0.1);
+
+        l.requestDeltaVerticalSpeed(-0.5);
+        l.requestDeltaHeading(0);
+        while(l.getPosition().y >= 1.5) {}
+
+        l.requestDeltaVerticalSpeed(0.4);
+        while(l.getPosition().y >= 1.05) {}
+
+        l.requestDeltaVerticalSpeed(0.1);
+
+        System.out.println("Test Finished: DView");
+
+        // Keep open:
+        while(true) {}
     }
 }
