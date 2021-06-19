8a8066bfd167c74e648cd9946b4af1ab8dbd0b91
Daniel Dunger <dad5056@thi.de>
2021-06-19 14:12:49 +0200

    todo

--- src/test/java/de/thi/dronesim/system/SystemTest.java
+++ src/test/java/de/thi/dronesim/system/SystemTest.java
@@ -1,6 +1,7 @@
 package de.thi.dronesim.system;
 
 import de.thi.dronesim.Simulation;
+import de.thi.dronesim.autopilot.AsyncAutopilot;
 import de.thi.dronesim.drone.Location;
 import de.thi.dronesim.gui.GuiManager;
 import org.junit.jupiter.api.Order;
@@ -9,9 +10,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Daniel Dunger
+ */
 public class SystemTest {
 
-    // The file to load for testing. They need to be in the folder "testconfigs"
+    // Test files are to be placed in "testconfigs"
+    // The file to load for testing
     private static final String FILENAME = "testconfig_1.json";
 
     // Test with graphical output
@@ -19,14 +24,14 @@ public class SystemTest {
     void TestSimulationDView()  {
         // Start Position: (0, 0, 10) -> Goal (0, 1, -10)
         Simulation sim = new Simulation("src/test/java/de/thi/dronesim/system/testconfigs/".concat(FILENAME));
-
+        assertNotNull(sim);
         sim.prepare();
+
         GuiManager gui = sim.getChild(GuiManager.class);
         assertNotNull(gui, "Could not get GuiManager.");
-
         gui.openDViewGui();
 
-        // Just waiting so it can properly load in, since it auto-starts atm.
+        // A forced wait to give the gui a chance to load up before running the sim
         try{
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -43,58 +48,22 @@ public class SystemTest {
         // at a fly-height of 5, with maximum speed and a heading of 90 degrees (eastwards)
 
         // Take off
-        l.requestDeltaVerticalSpeed(1);
-        while(l.getY() <= 4.4f) {}
-        l.requestDeltaVerticalSpeed(-0.9);
-        while(l.getY() <= 4.995f) {}
-        l.requestDeltaVerticalSpeed(-0.1);
-
-        // Heading
-        l.requestDeltaHeading(135);
-        while(l.getHeading() != 135) {}
-
-        // Speed up towards target:
-        l.requestDeltaAirspeed(2);
-        while(l.getPosition().x >= -8 || l.getPosition().z >= -8) {}
-
-        // Slow down towards Target
-        l.requestDeltaAirspeed(-1.5);
-        while(l.getPosition().x >= -9.5 || l.getPosition().z >= -9.5) {}
-
-        l.requestDeltaAirspeed(-0.4);
-        while(l.getPosition().x >= -10 || l.getPosition().z >= -10) {}
-
-        l.requestDeltaAirspeed(-0.1);
-
-        l.requestDeltaVerticalSpeed(-0.5);
-        l.requestDeltaHeading(0);
-        while(l.getPosition().y >= 1.5) {}
-
-        l.requestDeltaVerticalSpeed(0.4);
-        while(l.getPosition().y >= 1.05) {}
-
-        l.requestDeltaVerticalSpeed(0.1);
-
-        System.out.println("Test Finished: DView");
-
-        // Keep open:
-        while(true) {}
+        // TODO: Autopilot
     }
 
-    // Test without graphical output. Basically the same as above!
+    // Test without graphical output. 1:1 the same actions as above. Results should be equal
     @Test
     void TestSimulationMView() {
         // Test normal config
         Simulation sim = new Simulation("src/test/java/de/thi/dronesim/system/testconfigs/".concat(FILENAME));
+        assertNotNull(sim);
         sim.prepare();
 
         GuiManager gui = sim.getChild(GuiManager.class);
         assertNotNull(gui, "Could not get GuiManager");
-
         gui.openMViewGui();
 
-        // Below is pretty much the same as above, just without the graphical output.
-        // Just waiting so it can properly load in, since it auto-starts atm.
+        // A forced wait to give the gui a chance to load up before running the sim
         try{
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -110,42 +79,6 @@ public class SystemTest {
         // The following attempts to fly the drone from (0, 0, 10) to (0, 0, -10)
         // at a fly-height of 5, with maximum speed and a heading of 90 degrees (eastwards)
 
-        //Take off
-        l.requestDeltaVerticalSpeed(1);
-        while(l.getY() <= 4.4f) {}
-        l.requestDeltaVerticalSpeed(-0.9);
-        while(l.getY() <= 4.995f) {}
-        l.requestDeltaVerticalSpeed(-0.1);
-
-        // Heading
-        l.requestDeltaHeading(135);
-        while(l.getHeading() != 135) {}
-
-        // Speed up towards target:
-        l.requestDeltaAirspeed(2);
-        while(l.getPosition().x >= -8 || l.getPosition().z >= -8) {}
-
-        // Slow down towards Target
-        l.requestDeltaAirspeed(-1.5);
-        while(l.getPosition().x >= -9.5 || l.getPosition().z >= -9.5) {}
-
-        l.requestDeltaAirspeed(-0.4);
-        while(l.getPosition().x >= -10 || l.getPosition().z >= -10) {}
-
-        l.requestDeltaAirspeed(-0.1);
-
-        l.requestDeltaVerticalSpeed(-0.5);
-        l.requestDeltaHeading(0);
-        while(l.getPosition().y >= 1.5) {}
-
-        l.requestDeltaVerticalSpeed(0.4);
-        while(l.getPosition().y >= 1.05) {}
-
-        l.requestDeltaVerticalSpeed(0.1);
-
-        System.out.println("Test Finished: MView");
-
-        // Keep open:
-        while(true) {}
+        // TODO: Autopilot
     }
 }
