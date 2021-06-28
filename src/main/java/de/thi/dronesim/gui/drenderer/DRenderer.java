cdc7bf6b23f9fe20c8d0c6967400c48fe308f677
Molodos <michael-weichenrieder@t-online.de>
2021-06-28 22:16:40 +0200

    Import clear

--- src/main/java/de/thi/dronesim/gui/drenderer/DRenderer.java
+++ src/main/java/de/thi/dronesim/gui/drenderer/DRenderer.java
@@ -1,7 +1,6 @@
 package de.thi.dronesim.gui.drenderer;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.asset.plugins.FileLocator;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
